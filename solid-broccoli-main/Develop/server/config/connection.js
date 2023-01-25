165219de35b845961eabd62877b701431921e206
codybronsell <110582217+codybronsell@users.noreply.github.com>
2023-01-24 18:59:23 -0600

    added dependences

--- solid-broccoli-main/Develop/server/config/connection.js
+++ solid-broccoli-main/Develop/server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://admin:cody1234567@codybook.ongte2l.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
